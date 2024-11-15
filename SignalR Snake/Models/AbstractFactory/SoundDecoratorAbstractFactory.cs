06462aa24253bc1e6d9a43f51d203d6f346d49b6
Andzelas Gvazdauskas <andzelas.gvazdauskas@gmail.com>
2024-11-15 13:56:44 +0200

    Added Command DP, Projectiles, shooting action

---
+++ SignalR Snake/Models/AbstractFactory/SoundDecoratorAbstractFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalR_Snake.Models.AbstractFactory
+{
+    public abstract class SoundDecoratorAbstractFactory
+    {
+        public abstract void CreateBasicSnake(string name);
+        public abstract void CreateTriangleSnake(string name);
+        public abstract void CreateSquareSnake(string name);
+        public abstract void CreateRandomSnake(string name);
+    }
+}
